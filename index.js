8ad64ffe10b07e386aa68775dfb11dabb084e4d0
Mehedihasan023 <mh20973@gmail.com>
2023-08-24 23:44:49 +0600

    delete operation added

--- index.js
+++ index.js
@@ -62,6 +62,12 @@ async function run() {
             const result = await bookingColletion.insertOne(booking)
             res.send(result);
         })
+        app.delete('/bookings/:id', async(req,res)=>{
+          const id = req.params.id;
+          const query = {_id: new ObjectId(id)}
+          const result = await bookingColletion.deleteOne(query)
+          res.send(result)
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
