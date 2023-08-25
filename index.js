14961b1ee0f255ef275c5fea27bd580f0f2b9a7f
Mehedihasan023 <mh20973@gmail.com>
2023-08-26 00:37:03 +0600

    jwt verify token added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt= require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
@@ -20,6 +21,24 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
+//verify JWT
+const verifyJWT = (req,res,next)=>{
+    console.log('hitting verify JWT')
+    console.log(req.headers.authorization);
+    const authorization = req.headers.authorization;
+    if(!authorization){
+        return res.status(401).send({error: true, message: 'unauthorized access'})
+    }
+    const token = authorization.split(' ')[1];
+    console.log('token inside verify jwt',token);
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error,decoded)=>{
+        if(error){
+            return res.status(403).send({error: true, message: 'unauthorized access'})
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 async function run() {
     try {
@@ -28,7 +47,17 @@ async function run() {
 
         const serviceColletion = client.db('carDoctor').collection('services');
         const bookingColletion = client.db('carDoctor').collection('bookings');
+        //jwt
+        app.post('/jwt',(req,res)=>{
+            const user = req.body;
+            console.log(user);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{
+                expiresIn:'1h'});
+             res.send({token})
+           
+        })
 
+        //services routes
         app.get('/services', async (req, res) => {
             const cursor = serviceColletion.find();
             const result = await cursor.toArray();
@@ -45,9 +74,9 @@ async function run() {
             const result = await serviceColletion.findOne(query, options)
             res.send(result);
         })
-        // bookings
-        app.get('/bookings', async (req, res) => {
-            // console.log(req.query.email)
+        // bookings routes
+        app.get('/bookings',verifyJWT, async (req, res) => {
+           console.log('came back after verify')
             let query = {}
             if (req.query?.email) {
                 query = { email: req.query.email }
